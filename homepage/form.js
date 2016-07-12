a2c7c5a77cb6c3dc919be1dc28c3e18dea8b3e28
Bogdan Fusa <bfusa@endava.com>
2016-07-12 17:50:13 +0300

    validare si pentru email

--- homepage/form.js
+++ homepage/form.js
@@ -4,4 +4,10 @@ function validateForm() {
         alert("Name must be filled out");
         return false;
     }
+    var x = document.forms["frm1"]["email"].value;
+    if (x == null || x == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+
 }
